adb65c16301941f329f36c19bf4e39a56a7ad270
Basile Masson <basilemasson2@gmail.com>
2020-02-12 17:07:50 +0100

    done raking, now it must look good

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Epicure = Restaurant.new(name: "Epicure", address:"2 rue de Paris", category: "french")
+PickItUP = Restaurant.new(name: "PickItUP", address:"3 rue de Paris",category: "belgian")
+ElCamion = Restaurant.new(name: "ElCamion", address:"4 rue de Paris",category: "french")
+Chinatown = Restaurant.new(name: "PickItUP", address:"3 rue de Paris",category: "chinese")
+Epicure.save!
+PickItUP.save!
+ElCamion.save!
+Chinatown.save!
+
