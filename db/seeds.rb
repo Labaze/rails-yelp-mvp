9ea55f6503ebcb7d5b6d5e13e3ce2fba2a774136
Basile Masson <basilemasson2@gmail.com>
2020-02-12 17:56:07 +0100

    done with cool UI and UX

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+Review.destroy_all
+puts "Creating restaurants..."
+dishoom = {
+    name: "Dishoo",
+    address: "7 Boundary St, London E2 7JE",
+    category: "belgian",
+    phone_number: "0985367"
+  }
+pizza_east = {
+    name: "Pizza East",
+    address: "56A Shoreditch High St, London E1 6PQ",
+    category: "italian",
+    phone_number: "34567890"
+  }
+restaurant1 = Restaurant.create!(dishoom)
+restaurant2 = Restaurant.create!(pizza_east)
+puts "Creating reviews.."
+verry_good = {
+    content: "verry good",
+    rating: 5,
+    restaurant: restaurant1
+  }
+Review.create!(verry_good)
+ good = {
+    content: "good",
+    rating: 4,
+    restaurant: restaurant1
+  }
+Review.create!(good)
+middle = {
+    content: "middle",
+    rating: 3,
+    restaurant: restaurant2
+  }
+Review.create!(middle)
+puts "Finished!"
+
 
 Epicure = Restaurant.new(name: "Epicure", address:"2 rue de Paris", category: "french")
 PickItUP = Restaurant.new(name: "PickItUP", address:"3 rue de Paris",category: "belgian")
@@ -14,4 +53,5 @@ Epicure.save!
 PickItUP.save!
 ElCamion.save!
 Chinatown.save!
+
 
